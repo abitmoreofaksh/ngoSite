9b9b1af045ec9def947e82f5d24b063322597282
Arshdeep2402 <arsh.nike12345@gmail.com>
2024-05-12 13:01:01 +0530

    consultancies/students page

---
+++ consult-stu.php
@@ -0,0 +1,70 @@
+<?php include "includes/header.php"; ?>
+<link rel="stylesheet" href="consult-stu.css">
+
+<!--  ************************* Page Title Starts Here ************************** -->
+
+<div class="page-nav no-margin row">
+    <div class="container">
+        <div class="row">
+            <h2>CONSULTANCIES/ STUDENTS</h2>
+            <ul>
+                <li> <a href="#"><i class="fas fa-home"></i> Home</a></li>
+                <li><i class="fas fa-angle-double-right"></i> CONSULTANCIES/ STUDENTS </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!--  *************************  CONSULTANCIES/ STUDENTS Starts Here ************************** -->
+
+<section id="faq-2" class="faq-2 section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>FOR CONSULTANCIES/ STUDENTS</h2>
+  <p>We understand that it is important for aspiring students to build a brilliant profile when they want to pursue their higher studies abroad. In order to help enhance their profiles, Hamari Pahchan provides a Social Entrepreneurship Internship. We kindly invite you to fill the forms for more details.</p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-9 mx-auto">
+     
+      <div id="accordionExample" class="accordion shadow">
+
+            <div class="">
+            <div id="headingOne" class="  shadow-sm border-0">
+                <h2 class="mb-0"><a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="d-block position-relative text-dark text-uppercase collapsible-link py-2">Consultancy</a></h2>
+            </div>
+            <div id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample" class="collapse show">
+                <div class="card-body p-5">
+                <p class="font-weight-light m-0"> Consultancy representatives or their students who are interested to in our internship program, please fill the form here.</p>
+                </div>
+            </div>
+            </div>
+
+        
+            <div class="">
+            <div id="headingTwo" class="shadow-sm border-0">
+                <h2 class="mb-0 "><a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Students</a></h2>
+            </div>
+            <div id="collapseTwo" aria-labelledby="headingTwo" data-parent="#accordionExample" class="collapse">
+                <div class="card-body p-5">
+                <p class="font-weight-light m-0">Students who want to learn, connect and enhance their profiles through this internship program, please fill the form here.</p>
+                </div>
+            </div>
+            </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</section>
+
+
+
+
+<!--  ************************* Footer Starts Here ************************** -->
+
+<?php include "includes/footer.php"; ?>
